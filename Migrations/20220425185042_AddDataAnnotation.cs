547cba3ea9a518b6e04526d2abc9c08af0db00f6
waleery <walenda1997@gmail.com>
2022-04-25 20:52:01 +0200

    Dodanie adnotacji do klasy Person

---
+++ Migrations/20220425185042_AddDataAnnotation.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotNetDependencyInjection.Migrations
+{
+    public partial class AddDataAnnotation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Person",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Person",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Person",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Person",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
--- Models/Person.cs
+++ Models/Person.cs
@@ -1,9 +1,19 @@
-﻿namespace dotNetDependencyInjection.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dotNetDependencyInjection.Models
 {
     public class Person
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Column(TypeName="varchar(100)")]
         public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string LastName { get; set; }
     }
 
