1e591bfd73053c962108e4b88f4e8008bad8f295
waleery <walenda1997@gmail.com>
2022-04-25 20:58:04 +0200

    Dodanie zapisywania do bazy

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -20,7 +20,23 @@ namespace dotNetDependencyInjection.Pages
 
         public void OnGet()
         {
-            People = _context.Person.ToList();
+            People = _context.Person.Where(p => p.FirstName == "Patryk").ToList();
         }
+
+
+        [BindProperty]
+        public Person Person { get; set; }
+        public IActionResult OnPost()
+        {
+            People = _context.Person.ToList();
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _context.Person.Add(Person);
+            _context.SaveChanges();
+            return RedirectToPage("./Index");
+        }
+
     }
 }
