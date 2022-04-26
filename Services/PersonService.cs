08be19a02bc6cb5807650100fa940a59144000e7
waleery <walenda1997@gmail.com>
2022-04-26 22:33:52 +0200

    Stworzenie personService i  konstrukora ustawiajacego repozytorium

---
+++ Services/PersonService.cs
@@ -0,0 +1,21 @@
+﻿using dotNetDependencyInjection.Interfaces;
+using dotNetDependencyInjection.ViewModels.Person;
+
+namespace dotNetDependencyInjection.Services
+{
+    public class PersonService : IPersonService
+    {
+        private readonly IPersonRepository _personRepo;
+
+        public PersonService(IPersonRepository personRepo)
+        {
+            _personRepo = personRepo;
+        }
+
+        public ListPersonForListVM GetPeopleForList()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
