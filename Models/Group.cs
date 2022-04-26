3ebce7160be6439567568dc89fada8ba6e07c550
waleery <walenda1997@gmail.com>
2022-04-26 16:39:05 +0200

    Dodanie klasy Group i tabeli pośredniczącej PersonGroup

---
+++ Models/Group.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotNetDependencyInjection.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
---
+++ Models/PersonGroup.cs
@@ -0,0 +1,14 @@
+﻿namespace dotNetDependencyInjection.Models
+{
+    public class PersonGroup
+    {
+        public int PersonId { get; set; } //klucz obcy do Person
+
+        public Person Person { get; set; }
+
+        public int GroupId { get; set; } //klucz obcy do Group
+
+        public Group Group { get; set; }
+
+    }
+}
