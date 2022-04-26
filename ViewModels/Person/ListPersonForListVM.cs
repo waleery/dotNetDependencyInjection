e01d82311cb2d3f7f29b86fa9250fe6851248273
waleery <walenda1997@gmail.com>
2022-04-26 22:22:42 +0200

    Stworzenie klas PersonForListVM i ListPersonForListVM

---
+++ ViewModels/Person/ListPersonForListVM.cs
@@ -0,0 +1,8 @@
+﻿namespace dotNetDependencyInjection.ViewModels.Person
+{
+    public class PersonForListVm
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+    }
+}
---
+++ ViewModels/Person/PersonForListVm.cs
@@ -0,0 +1,8 @@
+﻿namespace dotNetDependencyInjection.ViewModels.Person
+{
+    public class PersonForListVM
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+    }
+}
