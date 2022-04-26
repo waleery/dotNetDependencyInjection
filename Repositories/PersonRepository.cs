30e01293067fe7e3066ed4a29d60e7b96d163eef
waleery <walenda1997@gmail.com>
2022-04-26 22:44:56 +0200

    Stworzeni repozytorium zwiazanego z EntityFramweorkCore

---
+++ Repositories/PersonRepository.cs
@@ -0,0 +1,22 @@
+﻿using dotNetDependencyInjection.Data;
+using dotNetDependencyInjection.Interfaces;
+using dotNetDependencyInjection.Models;
+
+namespace dotNetDependencyInjection.Repositories
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private readonly PeopleContext _context;
+
+        public PersonRepository(PeopleContext context)
+        {
+            _context = context;
+        }
+
+
+        public IQueryable<Person> GetAllActivePeople()
+        {
+            return _context.Person.Where(p => p.IsActive);
+        }
+    }
+}
