a1a55bb98300b8c85eebacb5776fbd6854875c56
waleery <walenda1997@gmail.com>
2022-04-26 17:17:31 +0200

    Implementacja klasy serwisu

---
+++ Services/PersonService.cs
@@ -0,0 +1,22 @@
+﻿using dotNetDependencyInjection.Data;
+using dotNetDependencyInjection.Interfaces;
+using dotNetDependencyInjection.Models;
+
+namespace dotNetDependencyInjection.Services
+{
+    public class PersonService : IPersonService
+    {
+        private readonly PeopleContext _context;
+
+        public PersonService(PeopleContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Person> GetActivePeople()
+        {
+            return _context.Person.Where(p => p.IsActive);
+        }
+
+    }
+}
