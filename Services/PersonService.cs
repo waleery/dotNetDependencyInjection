2f55eadec331c65bc89fb5270e04b8c7c6bfcc05
waleery <walenda1997@gmail.com>
2022-04-26 22:40:16 +0200

    pobieranie i mapowanie aktywnych osob z repozytorium

--- Services/PersonService.cs
+++ Services/PersonService.cs
@@ -14,8 +14,24 @@ namespace dotNetDependencyInjection.Services
 
         public ListPersonForListVM GetPeopleForList()
         {
-            throw new NotImplementedException();
+            var people = _personRepo.GetAllActivePeople();
+            ListPersonForListVM result = new ListPersonForListVM();
+            result.People = new List<PersonForListVM>();
+            foreach (var person in people)
+            {
+                // mapowanie obiektow
+                var pVM = new PersonForListVM()
+                {
+                    Id = person.Id,
+                    FullName = person.FirstName + " " +person.LastName
+                };
+                result.People.Add(pVM);
+            }
+            result.Count = result.People.Count;
+            return result;
         }
 
     }
+
 }
+
