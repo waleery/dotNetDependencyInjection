9791c1b5f5d749d06b580eb5074747307ec8114f
waleery <walenda1997@gmail.com>
2022-04-26 16:27:25 +0200

    Dodanie klasy Address

---
+++ Models/Address.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dotNetDependencyInjection.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(100)]
+        public string Street { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
+        public string City { get; set; }
+        
+        [Required]
+        [MaxLength(6)]
+        [Column(TypeName = "varchar(6)")]
+        public string ZipCode { get; set; }
+        
+        [Required]
+        [MaxLength(2)]
+        public string Country { get; set; }
+    }
+
